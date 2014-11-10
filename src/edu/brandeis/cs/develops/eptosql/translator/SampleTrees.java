80b85bd53f84ebad40789a1e9cb746533292c42a
Brionne Godby <B.Godby+bitbucket@gmail.com>
2014-11-10 11:52:19 -0500

    Initial fully nested sql generation. Incomplete unnesting section included.

--- src/edu/brandeis/cs/develops/eptosql/translator/SampleTrees.java
+++ src/edu/brandeis/cs/develops/eptosql/translator/SampleTrees.java
@@ -16,4 +16,17 @@ public class SampleTrees {
 	public static Relation TwoJoinSelectionExpression() {
 		return new Join(BasicJoinExpression(), BasicSelectionExpression(), "att5=att5", "MergeJoin");
 	}
+        
+   
+        
+        public static void main(String[] args) {
+            Relation employees = new Table("employees");
+            Relation employers = new Table("employers");
+            Relation deparments = new Table("departments");
+            Relation depToEmp = new Table("employees_to_department");
+            Relation emp_employees = new Join(employees, employers, "yee_employer_id=yer_employer_id", "INNER HASH");
+            Relation emp_dep = new Join(depToEmp, deparments, "dep_department_id=etd_department_id", "INNER HASH");
+            Relation full = new Join(emp_employees, emp_dep, "yee_employee_id=etd_employee_id", "INNER HASH");
+            System.out.println(SQLGenerator.createSQL(full));
+        }
 }
