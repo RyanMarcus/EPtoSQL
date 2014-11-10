e898377f43bbdb92a05fa660d30b0ddd7fef9ed7
Ryan Marcus <ryan@rmarcus.info>
2014-11-10 14:27:47 -0500

    added tests and stream reading to parser

--- src/edu/brandeis/cs/develops/eptosql/parser/Parser.java
+++ src/edu/brandeis/cs/develops/eptosql/parser/Parser.java
@@ -1,5 +1,7 @@
 package edu.brandeis.cs.develops.eptosql.parser;
 
+import java.io.InputStream;
+
 import edu.brandeis.cs.develops.eptosql.parser.lexer.Lexer;
 import edu.brandeis.cs.develops.eptosql.parser.parser.ShiftReduceParser;
 import edu.brandeis.cs.develops.eptosql.parser.parser.AST.ASTNode;
@@ -8,5 +10,10 @@ public class Parser {
 	public ASTNode parseString(String s) {
 		ShiftReduceParser srp = new ShiftReduceParser();
 		return srp.parseTokens(Lexer.createLexerForString(s));
+	}
+	
+	public ASTNode parseStream(InputStream s) {
+		ShiftReduceParser srp = new ShiftReduceParser();
+		return srp.parseTokens(Lexer.createLexerForInputStream(s));
 	}
 }
--- src/edu/brandeis/cs/develops/eptosql/parser/ParserTest.java
+++ src/edu/brandeis/cs/develops/eptosql/parser/ParserTest.java
@@ -2,6 +2,8 @@ package edu.brandeis.cs.develops.eptosql.parser;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -41,6 +43,23 @@ public class ParserTest {
 	@Test
 	public void test4() {
 		EP ep = (EP) p.parseString("PNLJOIN(s_nationkey = n_nationkey, PMJOIN(ps_suppkey = s_suppkey, PTABLE(PS), PTABLE(S)), PSELECT(n_name = 'ASIA', PTABLE(N)))");
+		assertTrue(ep.expr.join.join_type.type.equals("PNLJOIN"));
+		assertTrue(ep.expr.join.paren_string_expr_expr.string.equals("s_nationkey = n_nationkey"));
+		assertTrue(ep.expr.join.paren_string_expr_expr.double_comma_expr.expr1.expr.join.join_type.type.equals("PMJOIN"));
+		assertTrue(ep.expr.join.paren_string_expr_expr.double_comma_expr.expr1.expr.join.paren_string_expr_expr.double_comma_expr.expr1.expr.table.paren_string.string.equals("PS"));
+		assertTrue(ep.expr.join.paren_string_expr_expr.double_comma_expr.expr1.expr.join.paren_string_expr_expr.double_comma_expr.expr2.expr.table.paren_string.string.equals("S"));
+		assertTrue(ep.expr.join.paren_string_expr_expr.double_comma_expr.expr2.expr.selection.paren_string_expr.string.equals("n_name = 'ASIA'"));
+		assertTrue(ep.expr.join.paren_string_expr_expr.double_comma_expr.expr2.expr.selection.paren_string_expr.expr.expr.table.paren_string.string.equals("N"));
+
+	}
+	
+	@Test
+	public void test5() {
+		String s = "PNLJOIN(s_nationkey = n_nationkey, PMJOIN(ps_suppkey = s_suppkey, PTABLE(PS), PTABLE(S)), PSELECT(n_name = 'ASIA', PTABLE(N)))";
+		
+		ByteArrayInputStream in = new ByteArrayInputStream(s.getBytes());
+		
+		EP ep = (EP) p.parseStream(in);
 		assertTrue(ep.expr.join.join_type.type.equals("PNLJOIN"));
 		assertTrue(ep.expr.join.paren_string_expr_expr.string.equals("s_nationkey = n_nationkey"));
 		assertTrue(ep.expr.join.paren_string_expr_expr.double_comma_expr.expr1.expr.join.join_type.type.equals("PMJOIN"));
