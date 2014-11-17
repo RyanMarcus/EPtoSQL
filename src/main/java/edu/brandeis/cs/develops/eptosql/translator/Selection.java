021e406ef8d5c8025b052c820e2f74f034056700
rachel <rayleemamunk@gmail.com>
2014-11-17 12:38:43 -0500

    Started adding doctyping

--- src/main/java/edu/brandeis/cs/develops/eptosql/translator/Selection.java
+++ src/main/java/edu/brandeis/cs/develops/eptosql/translator/Selection.java
@@ -1,5 +1,9 @@
 package edu.brandeis.cs.develops.eptosql.translator;
 
+/**
+ * @author Rachel Leeman-Munk
+ *
+ */
 public class Selection extends Relation{
 	private Relation leftChild;
 	private String predicate;
@@ -8,26 +12,45 @@ public class Selection extends Relation{
 		this.setPredicate(predicate);
 	}
 	
+	/**
+	 * @return left child of selection
+	 */
 	public Relation getChild() {
 		return leftChild;
 	}
 	
+	/**
+	 * @param leftChild selection child
+	 */
 	public void setChild(Relation leftChild) {
 		this.leftChild = leftChild;
 	}
 	
+	/**
+	 * @return selection predicate
+	 */
 	public String getPredicate() {
 		return predicate;
 	}
 	
+	/**
+	 * @param predicate selection predicate
+	 */
 	public void setPredicate(String predicate) {
 		this.predicate = predicate;
 	}
 	
+	/**
+	 * @param predicate the selection predicate being compared
+	 * @return boolean - this.predicate == parameter predicate
+	 */
 	public Boolean hasAttributes(String predicate) {
 		return this.predicate.equals(predicate);
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	public String toString() {
 		return "Select " + this.getPredicate() + " from " + this.getChild().toString();
 	}
