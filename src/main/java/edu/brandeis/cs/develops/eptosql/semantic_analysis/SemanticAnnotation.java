9c5ec3ba4de63e726f5e6fad0c3431dd3ef34df5
Ryan Marcus <ryan@rmarcus.info>
2014-11-17 12:57:01 -0500

    initial draft of selection/join issues

---
+++ src/main/java/edu/brandeis/cs/develops/eptosql/semantic_analysis/SemanticAnnotation.java
@@ -0,0 +1,9 @@
+package edu.brandeis.cs.develops.eptosql.semantic_analysis;
+
+public class SemanticAnnotation {
+	public String message;
+	
+	public SemanticAnnotation(String s) {
+		message = s;
+	}
+}
