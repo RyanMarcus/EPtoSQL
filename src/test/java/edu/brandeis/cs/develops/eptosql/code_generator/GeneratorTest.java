273c891f75b995a03cf9bf89c9de38316f014c60
Ryan Marcus <ryan@rmarcus.info>
2014-11-25 10:01:02 -0500

    Added some very simple substring tests for the code generator

--- src/test/java/edu/brandeis/cs/develops/eptosql/code_generator/GeneratorTest.java
+++ src/test/java/edu/brandeis/cs/develops/eptosql/code_generator/GeneratorTest.java
@@ -1,20 +1,82 @@
 package edu.brandeis.cs.develops.eptosql.code_generator;
 
-import edu.brandeis.cs.develops.eptosql.translator.Relation;
-import edu.brandeis.cs.develops.eptosql.translator.SampleTrees;
 import static org.junit.Assert.assertTrue;
 
-
-import org.junit.Before;
 import org.junit.Test;
+
+import edu.brandeis.cs.develops.eptosql.parser.Parser;
+import edu.brandeis.cs.develops.eptosql.parser.parser.ParserException;
+import edu.brandeis.cs.develops.eptosql.parser.parser.AST.EP;
+import edu.brandeis.cs.develops.eptosql.translator.ASTTranslator;
+import edu.brandeis.cs.develops.eptosql.translator.Relation;
+import edu.brandeis.cs.develops.eptosql.translator.SampleTrees;
 
 public class GeneratorTest {
 
     @Test
-    public void dummy() {
+    public void test1() {
         Relation test = SampleTrees.EmployeeDepartmentExpression();
         String SQL = SQLGenerator.createUnnestedSQL(test);
-        //System.out.println(SQL);
-        assertTrue("Dummy Test", true);
+        assertTrue(SQL.indexOf("departments") != -1);
+        assertTrue(SQL.indexOf("SELECT") != -1);
+        assertTrue(SQL.indexOf("JOIN") != -1);
+        assertTrue(SQL.indexOf("HASH") != -1);
+        assertTrue(SQL.indexOf("yee_employer_id=yer_employer_id") != -1);        
+    }
+    
+    @Test
+    public void test2() {
+        Relation test = SampleTrees.EmployeeDepartmentExpression();
+        String SQL = SQLGenerator.createNestedSQL(test);
+        assertTrue(SQL.indexOf("departments") != -1);
+        assertTrue(SQL.indexOf("SELECT") != -1);
+        assertTrue(SQL.indexOf("JOIN") != -1);
+        assertTrue(SQL.indexOf("HASH") != -1);
+        assertTrue(SQL.indexOf("yee_employer_id=yer_employer_id") != -1); 
+    }
+    
+    @Test
+    public void test3() throws ParserException {
+    	Parser p = new Parser();
+    	EP ep = (EP) p.parseString("PSELECT(n_name = 'ASIA', PTABLE(N))");
+		ASTTranslator astt = new ASTTranslator();
+		Relation r = astt.parse(ep);
+        String SQL = SQLGenerator.createUnnestedSQL(r);
+
+        assertTrue(SQL.indexOf("SELECT") != -1);
+        assertTrue(SQL.indexOf("ASIA") != -1);
+    }
+    
+    @Test
+    public void test4() throws ParserException {
+    	Parser p = new Parser();
+    	EP ep = (EP) p.parseString("PSELECT(n_name = 'ASIA', PTABLE(N))");
+		ASTTranslator astt = new ASTTranslator();
+		Relation r = astt.parse(ep);
+        String SQL = SQLGenerator.createUnnestedSQL(r);
+
+        assertTrue(SQL.indexOf("SELECT") != -1);
+        assertTrue(SQL.indexOf("ASIA") != -1);
+    }
+    
+    
+    @Test
+    public void test5() throws ParserException {
+    	Parser p = new Parser();
+    	EP ep = (EP) p.parseString("PNLJOIN(s_nationkey = n_nationkey, PMJOIN(ps_suppkey = s_suppkey, PTABLE(PS), PTABLE(S)), PSELECT(n_name = 'ASIA', PTABLE(N)))");
+		ASTTranslator astt = new ASTTranslator();
+		Relation r = astt.parse(ep);
+        String SQL = SQLGenerator.createUnnestedSQL(r);
+
+        assertTrue(SQL.indexOf("SELECT") != -1);
+        assertTrue(SQL.indexOf("ASIA") != -1);
+        assertTrue(SQL.indexOf("LOOP") != -1);
+        assertTrue(SQL.indexOf("MERGE") != -1);
+        assertTrue(SQL.indexOf("s_nationkey") != -1);
+        assertTrue(SQL.indexOf("ps_suppkey") != -1);
+
+
+
+
     }
 }
