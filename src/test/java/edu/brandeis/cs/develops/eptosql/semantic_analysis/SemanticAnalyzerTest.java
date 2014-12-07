3338860f3e66343e7494ad37426d823b34844e5c
Ryan Marcus <ryan@rmarcus.info>
2014-12-07 13:44:29 -0500

    Added copyright notice

--- src/test/java/edu/brandeis/cs/develops/eptosql/semantic_analysis/SemanticAnalyzerTest.java
+++ src/test/java/edu/brandeis/cs/develops/eptosql/semantic_analysis/SemanticAnalyzerTest.java
@@ -1,3 +1,23 @@
+/*
+Copyright 2014 Brionne Godby, Rachel Leeman-Munk, Ryan Marcus
+
+This file is part of EPtoSQL.
+
+EPtoSQL is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+EPtoSQL is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with EPtoSQL.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 package edu.brandeis.cs.develops.eptosql.semantic_analysis;
 
 import static org.junit.Assert.assertTrue;
