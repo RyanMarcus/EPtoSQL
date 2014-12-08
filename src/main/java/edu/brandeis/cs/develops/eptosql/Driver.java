613838aaede00f63dfe04fc95178038920dcdb09
rachel <rayleemamunk@gmail.com>
2014-12-08 14:29:34 -0500

    Changed name of Driver

--- src/main/java/edu/brandeis/cs/develops/eptosql/Driver.java
+++ src/main/java/edu/brandeis/cs/develops/eptosql/Driver.java
@@ -20,28 +20,11 @@ along with EPtoSQL.  If not, see <http://www.gnu.org/licenses/>.
 
 package edu.brandeis.cs.develops.eptosql;
 
-import java.io.ByteArrayInputStream;
-import java.util.Scanner;
-
-import edu.brandeis.cs.develops.eptosql.frontend.CodeGenerationOption;
-import edu.brandeis.cs.develops.eptosql.frontend.EPtoSQL;
-import edu.brandeis.cs.develops.eptosql.frontend.IROption;
+import java.io.IOException;
 
 public class Driver {
 
-	public static void main(String[] args) {
-		CodeGenerationOption cgo = CodeGenerationOption.NESTED;
-		IROption iro = IROption.DISABLE;
-		
-		Scanner sc = new Scanner(System.in);
-		String plan;
-		while ((plan = sc.nextLine()) != null) {
-			EPtoSQL ets = new EPtoSQL();
-			ets.compile(cgo, iro, new ByteArrayInputStream(plan.getBytes()), System.out);
-		}
-
-		sc.close();
-
+	public static void main(String[] args) throws IOException {
+		new Cli(args).parse();
 	}
-
 }
